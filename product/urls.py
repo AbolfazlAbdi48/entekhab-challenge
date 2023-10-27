82debe0ffe04622874461a1807b3bf90ebfcd9ef
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-10-27 15:29:24 +0330

    Add: AdminProductList View & Template

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import admin_home_view
+from .views import admin_home_view, AdminProductListView
 
 app_name = 'product'
 urlpatterns = [
     path('panel/', admin_home_view, name='admin-home'),
+    path('panel/products/', AdminProductListView.as_view(), name='admin-product-list'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from django.views.generic import ListView
+
+from product.models import Product
+
+
 def admin_home_view(request):
     return render(request, 'product/admin/admin_home.html')
+
+
+class AdminProductListView(ListView):
+    model = Product
+    template_name = 'product/admin/admin_product_list.html'
+    paginate_by = 20
