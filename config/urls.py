00ed8c06fcd04d030e285464c07328e49f06bf79
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-10-27 18:06:49 +0330

    Add: home page view & product list

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from config import settings
 
 urlpatterns = [
     path('', include('product.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from product.models import Product
+
+
+def home_view(request):
+    products = Product.objects.filter(is_active=True)
+
+    context = {
+        'products': products
+    }
+    return render(request, 'core/home.html', context)
