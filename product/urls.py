9a970078bb914ae7c480742d83f100414ed6c57f
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-10-27 16:18:13 +0330

    Add: AdminProductList Update & Template

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import admin_home_view, AdminProductListView, AdminProductCreateView
+from .views import (
+    admin_home_view,
+    AdminProductListView,
+    AdminProductCreateView,
+    AdminProductUpdateView
+)
 
 app_name = 'product'
 urlpatterns = [
     path('panel/', admin_home_view, name='admin-home'),
     path('panel/products/', AdminProductListView.as_view(), name='admin-product-list'),
     path('panel/products/create', AdminProductCreateView.as_view(), name='admin-product-create'),
+    path('panel/products/update/<int:pk>', AdminProductUpdateView.as_view(), name='admin-product-update'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from product.models import Product
 
@@ -18,6 +18,13 @@ class AdminProductListView(ListView):
 
 
 class AdminProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/admin/admin_product_create_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('product:admin-product-list')
+
+
+class AdminProductUpdateView(UpdateView):
     model = Product
     template_name = 'product/admin/admin_product_create_update.html'
     fields = '__all__'
