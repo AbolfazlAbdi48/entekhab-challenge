3035e88d0441d6cbc002f89be721c31d148e0a77
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-10-28 16:08:02 +0330

    Add: cart view & template for show user cart detail

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home_view, ProductDetailView
+from .views import (
+    home_view,
+    cart_view,
+    ProductDetailView
+)
 
 app_name = 'core'
 urlpatterns = [
     path('', home_view, name='home'),
+    path('cart/', cart_view, name='cart'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import DetailView
 
+from core.cart import Cart
 from product.models import Product
 
 
@@ -13,6 +14,15 @@ def home_view(request):
         'products': products
     }
     return render(request, 'core/home.html', context)
+
+
+def cart_view(request):
+    cart = Cart(request)
+
+    context = {
+        'cart': cart
+    }
+    return render(request, 'core/cart.html', context)
 
 
 class ProductDetailView(DetailView):
