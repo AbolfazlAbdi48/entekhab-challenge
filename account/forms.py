99035bf58e9a3d824c61201b78c2ec3892cb0341
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-10-28 22:02:43 +0330

    refactor: Register Form Meta fields

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.models import User
 from django import forms
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(label='ایمیل')
-    first_name = forms.CharField(label='نام')
-    last_name = forms.CharField(label='نام خانوادگی')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
